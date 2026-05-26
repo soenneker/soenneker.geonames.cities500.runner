ae69857f4208d41cf02e64fe9543ce12361d3d99
Jake Soenneker <jake@soenneker.com>
2026-05-26 13:59:35 -0500

    misc

--- src/Soenneker.GeoNames.Cities500.Runner/Program.cs
+++ src/Soenneker.GeoNames.Cities500.Runner/Program.cs
@@ -1,34 +1,76 @@
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Soenneker.Enums.DeployEnvironment;
 using Soenneker.Extensions.LoggerConfiguration;
+using Soenneker.Extensions.ValueTask;
 
 namespace Soenneker.GeoNames.Cities500.Runner;
 
-public static class Program
+public sealed class Program
 {
+    private static string? _environment;
+
+    private static CancellationTokenSource? _cts;
+
     public static async Task Main(string[] args)
     {
-        IConfigurationRoot config = new ConfigurationBuilder()
-                                    .AddJsonFile("appsettings.json", true, true)
-                                    .AddEnvironmentVariables()
-                                    .AddCommandLine(args)
-                                    .Build();
+        _environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
-        Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(config)
-                                              .Enrich.FromLogContext()
-                                              .WriteTo.Console()
-                                              .CreateBootstrapLogger();
+        if (string.IsNullOrWhiteSpace(_environment))
+            throw new Exception("ASPNETCORE_ENVIRONMENT is not set");
 
-        await Host.CreateDefaultBuilder(args)
-                  .UseSerilog((context, configuration) => configuration.ConfigureLoggerSync(context.Configuration))
-                  .ConfigureServices((_, services) =>
-                  {
-                      services.AddSingleton<IConfiguration>(config);
-                      Startup.ConfigureServices(services);
-                  })
-                  .RunConsoleAsync();
+        // Declare CancellationTokenSource in a broader scope
+        _cts = new CancellationTokenSource(); // Use 'using' to ensure proper disposal
+        Console.CancelKeyPress += OnCancelKeyPress;
+
+        try
+        {
+            await CreateHostBuilder(args).RunConsoleAsync(_cts.Token);
+        }
+        catch (Exception e)
+        {
+            Log.Error(e, "Stopped program because of exception");
+            throw;
+        }
+        finally
+        {
+            Console.CancelKeyPress -= OnCancelKeyPress; // Detach the handler
+
+            _cts.Dispose();
+            await Log.CloseAndFlushAsync().NoSync();
+        }
+    }
+
+    /// <summary>
+    /// Used for WebApplicationFactory, cannot delete, cannot change access, cannot change number of parameters.
+    /// </summary>
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        DeployEnvironment envEnum = DeployEnvironment.FromName(_environment!);
+
+        LoggerConfigurationExtension.BuildBootstrapLoggerAndSetGloballySync(envEnum);
+
+        IHostBuilder? host = Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, builder) =>
+            {
+                builder.AddEnvironmentVariables();
+                builder.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath);
+
+                builder.Build();
+            })
+            .UseSerilog()
+            .ConfigureServices((_, services) => { Startup.ConfigureServices(services); });
+
+        return host;
+    }
+
+    private static void OnCancelKeyPress(object? sender, ConsoleCancelEventArgs eventArgs)
+    {
+        eventArgs.Cancel = true; // Prevents immediate termination
+        _cts?.Cancel();
     }
 }
